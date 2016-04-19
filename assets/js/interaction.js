051c8a6e9bc9731dee3535f4b6850922a25b1e9b
Ben Domanico <ben.domanico@appdirect.com>
2016-04-19 10:56:08 -0700

    Remove core Pure css (keep grid only)
    That's all I wanted anyways!

--- assets/js/interaction.js
+++ assets/js/interaction.js
@@ -2,12 +2,8 @@ $(document).ready(function(){
 
   $( ".thumbnail").on("mouseenter",function(){
     $('body').css("background-color",$(this).attr("data-color"));
-    console.log('mouseenter');
-    console.log(e.target, $(this).attr("data-color"));
-
   }).on("mouseleave",function(){
     $('body').css("background-color","#E2E2E2");
-    console.log('mouseOUT');
   });
 
 });
