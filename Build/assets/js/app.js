b88a4660951451dbe0fa76a904eec38b62e5dde6
Ben Domanico <ben.domanico@appdirect.com>
2016-04-19 23:47:50 -0700

    More tweakin' and fiddling.
    Added font
    Sticky Footer
    Cleaner Code

--- Build/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-  $( ".thumbnail").on("mouseenter",function(){
-    $('body').css("background-color",$(this).attr("data-color"));
-  }).on("mouseleave",function(){
-    $('body').css("background-color","#E2E2E2");
-  });
-
-});
--- Build/assets/js/interaction.js
+++ Build/assets/js/interaction.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
   $( ".thumbnail").on("mouseenter",function(){
-    $('body').css("background-color",$(this).attr("data-color"));
+    $('body, nav').css("background-color",$(this).attr("data-color"));
   }).on("mouseleave",function(){
-    $('body').css("background-color","#E2E2E2");
+    $('body, nav').css("background-color","#E2E2E2");
   });
 
 });
