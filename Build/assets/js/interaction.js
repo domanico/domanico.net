1f67665f20070f039f0ba1e1004a3d75b0dc65d0
Ben Domanico <ben.domanico@appdirect.com>
2016-04-22 08:57:05 -0700

    Added Calibre, clean up CSS
    On the path to renaming .tiles, much CSS cleaned up.

--- Build/assets/js/interaction.js
+++ Build/assets/js/interaction.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 
-  $( ".thumbnail").on("mouseenter",function(){
+  $( ".tile.project").on("mouseenter",function(){
     $('body, nav').css("background-color",$(this).attr("data-color"));
   }).on("mouseleave",function(){
     $('body, nav').css("background-color","#E2E2E2");
   });
 
 });
+
+$(document).pjax('a', '#pjax-container')
