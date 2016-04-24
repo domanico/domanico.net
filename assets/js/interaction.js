3e1466e793b4da68dea2591df4d3935b90a1f0fd
Ben Domanico <ben.domanico@appdirect.com>
2016-04-24 15:53:24 -0700

    This is me saying goodbye to the bg-color-on-hover thing.

--- assets/js/interaction.js
+++ assets/js/interaction.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
-  $( ".tile.project").on("mouseenter",function(){
+  $( ".project").on("mouseenter",function(){
     $('body, nav').css("background-color",$(this).attr("data-color"));
   }).on("mouseleave",function(){
-    $('body, nav').css("background-color","#E2E2E2");
+    $('body, nav').css("background-color","#202020");
   });
 
 });
